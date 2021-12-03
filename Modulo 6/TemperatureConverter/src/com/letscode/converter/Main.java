41bd6040b442c4012c8dcafa74d338b1114669eb
Marisa Sales <marisa_sales6@hotmail.com>
2021-12-03 18:17:43 -0300

    Update Main.java

--- Modulo 6/TemperatureConverter/src/com/letscode/converter/Main.java
+++ Modulo 6/TemperatureConverter/src/com/letscode/converter/Main.java
@@ -12,14 +12,14 @@ public class Main {
     System.out.print("Informe a temperatura em celsius: ");
     float celsius = input.nextFloat();
 
-    final float fahrenheit = (celsius * 1.8f) + 32;
-    final float kelvin = celsius + 273.15f;
-    final float reaumur = celsius * 0.8f;
-    final float rankine = (celsius * 1.8f) + 491.67f;
+    final float FAHRENHEIT = (celsius * 1.8f) + 32;
+    final float KELVIN = celsius + 273.15f;
+    final float REAUMUR = celsius * 0.8f;
+    final float RANKINE = (celsius * 1.8f) + 491.67f;
 
-    System.out.printf("18 ºC convertido para Fahrenheit é %.2f ºF %n", fahrenheit);
-    System.out.printf("18 ºC convertido para Kelvin é %.2f K %n", kelvin);
-    System.out.printf("18 ºC convertido para Réaumur é %.2f ºRé %n", reaumur);
-    System.out.printf("18 ºC convertido para Rankine %.2f ºRa %n", rankine);
+    System.out.printf("18 ºC convertido para Fahrenheit é %.2f ºF %n", FAHRENHEIT);
+    System.out.printf("18 ºC convertido para Kelvin é %.2f K %n", KELVIN);
+    System.out.printf("18 ºC convertido para Réaumur é %.2f ºRé %n", REAUMUR);
+    System.out.printf("18 ºC convertido para Rankine %.2f ºRa %n", RANKINE);
   }
 }
